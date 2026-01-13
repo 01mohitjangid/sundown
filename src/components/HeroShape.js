f7716a788e815e883abb23ba7fc9a27e20b00688
01mohitjangid <01mohitjangid@gmail.com>
2026-01-13 12:13:54 +0530

    Refactor HeroShape component to enhance structure and styling

--- src/components/HeroShape.js
+++ src/components/HeroShape.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 function HeroShape() {
   return (
-    <div id="hero-shape">
-      <div id="hero-1" />
-      <div id="hero-2" />
-      <div id="hero-3" />
+    <div className="absolute w-[46vw] h-[36vw] right-0 top-[65vh]">
+      <div className="bg-[#FE320A] h-full w-full mt-24 rounded-tl-[50%] rounded-bl-[50%] blur-[1px] absolute" />
+      <div className="bg-gradient-to-b from-[#FE320A] to-[#fe3f0a] h-[30vw] w-[30vw] rounded-full absolute blur-[25px] animate-hero2" />
+      <div className="bg-gradient-to-b from-[#FE320A] to-[#fe3f0a] h-[30vw] w-[30vw] rounded-full absolute blur-[25px] animate-hero1" />
     </div>
   );
 }
